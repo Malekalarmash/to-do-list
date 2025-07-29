b949f22ee68ef61412eff4b0d4a221c0c8a89c46
Malek Alarmash <81653238+Malekalarmash@users.noreply.github.com>
2025-07-29 03:49:32 -0400

    Assignment Week3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
+import { useState } from 'react'
 
 function App() {
-
+  const [newTodo, setNewTodo] = useState('Walk the Dog')
   return (
     <>
       <h1>My todos</h1>
       <TodoList />
+      <p>{newTodo}</p>
       <TodoForm />
     </>
   )
